8d2bac15e564fe8a7852d970388193b449d6e9c6
RedONe06 <andrielejorasdossantos@gmail.com>
2022-04-13 18:13:44 -0300

    final

--- js/orcamento.js
+++ js/orcamento.js
@@ -1,40 +1,3 @@
-//Mostrar a quantidade de litros necessários para pintar uma "casa"
-
-/* A casa:  ---------------
-parede esquerda:
-largura:
-altura:
-
-parede da frente:
-altura:
-largura:
-
-Porta:
-largura:
-altura:
-quantidade:
-
-Janela:
-largura:
-altura:
-quantidade:
-
-Outro objeto consideravel:
-largura:
-altura:
-quantidade:
-
-Cálculo: -------------
-
-litros = (areaTotal/rendimento)*quantidadeDemao
-valorFinal = litros * valorDaMarca
-
-Resposta: ---------------- 
-Olá 'nomeUsuario'! Para pintar seus 'areaTotal' metros quadrados
-você precisará de 'numeroDeLitros' dando um total de 'valorFinal'*/
-
-
-var larguraLateral = document.querySelector('#larguraLateral').value
 var larguraLateral = document.querySelector('#larguraLateral').value
 var alturaLateral = document.querySelector('#alturaLateral').value
 
@@ -54,25 +17,71 @@ var larguraOutros = document.querySelector('#larguraOutros').value
 var quantidadeOutros = document.querySelector('#quantidadeOutros').value
 
 var rendimento = document.querySelector('#rendimento').value
-var demao = document.querySelector('#demao').value
+var demao = document.querySelector('#demaos').value
 var resultado = document.querySelector('#resultado')
 
-var areaLateral = (larguraLateral*alturaLateral)*2
-var areaFrontal = (larguraFrontal*alturaFrontal)*2
-var areaPortas = (larguraPortas*alturaPortas)*quantidadePortas
-var areaJanelas = (larguraJanelas*alturaJanelas)*quantidadeJanelas
-var areaOutros = (larguraOutros*alturaOutros)*quantidadeOutros
+var corTh = document.querySelector('th')
+var corTinta = document.querySelector('#corTinta').value
 
-function calcularArea(){
-    var areaTotal = (areaLateral+areaFrontal)-(areaPortas+areaJanelas+areaOutros)
-    return areaTotal
+function areaLateral() {
+    larguraLateral = document.querySelector('#larguraLateral').value
+    alturaLateral = document.querySelector('#alturaLateral').value
 
+    var areaLateral = (larguraLateral * alturaLateral) * 2
+    return areaLateral
 }
 
-function calcularLitros(){
-    var litros = (calcularArea()/rendimento)*demao
-    alert(litros)
-    //resultado.innerHTML = "Você vai precisar de " + litros + "litros"
+function areaFrontal() {
+    larguraFrontal = document.querySelector('#larguraFrontal').value
+    alturaFrontal = document.querySelector('#alturaFrontal').value
+
+    var areaFrontal = (larguraFrontal * alturaFrontal) * 2
+    return areaFrontal
+}
+
+function areaPortas() {
+    alturaPortas = document.querySelector('#alturaPortas').value
+    larguraPortas = document.querySelector('#larguraPortas').value
+    quantidadePortas = document.querySelector('#quantidadePortas').value
+
+    var areaPortas = (larguraPortas * alturaPortas) * quantidadePortas
+    return areaPortas
+}
+
+function areaJanelas() {
+    alturaJanelas = document.querySelector('#alturaJanelas').value
+    larguraJanelas = document.querySelector('#larguraJanelas').value
+    quantidadeJanelas = document.querySelector('#quantidadeJanelas').value
+
+    var areaJanelas = (larguraJanelas * alturaJanelas) * quantidadeJanelas
+    return areaJanelas
+}
+
+function areaOutros() {
+    alturaOutros = document.querySelector('#alturaOutros').value
+    larguraOutros = document.querySelector('#larguraOutros').value
+    quantidadeOutros = document.querySelector('#quantidadeOutros').value
+
+    var areaOutros = (larguraOutros * alturaOutros) * quantidadeOutros
+    return areaOutros
+}
+
+function calcularArea() {
+    var areaTotal = (areaLateral() + areaFrontal()) - (areaPortas() + areaJanelas() + areaOutros())
+    return areaTotal
+}
+function trocarTinta(){
+    corTh = document.querySelector('th')
+    corTinta = document.querySelector('#corTinta').value
+
+    corTh.style.backgroundColor = corTinta
+}
+function calcularLitros() {
+    rendimento = document.querySelector('#rendimento').value
+    demao = document.querySelector('#demaos').value
+
+    var litros = (calcularArea() / rendimento) * demao
+    resultado.innerHTML = "Você vai precisar de " + litros + " litros"
 }
 
 
